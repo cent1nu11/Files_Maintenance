0ddbefd18c9a5fa95f488bc52bf1ac2cfc1cd761
cent1nu11 <rogerwu63@gmail.com>
2020-05-16 05:35:46 -0600

    added delete function

--- delete_files_older_than_x_days.py
+++ delete_files_older_than_x_days.py
@@ -1,3 +1,7 @@
+# this program will take delete any files in a directory (dest_path) that are older than
+# a certain number of days specified (number_of_days_old)
+# it does not delete any subdirectories within the main directory
+
 import os
 from datetime import datetime, timedelta
 import time
@@ -5,8 +9,7 @@ import time
 dest_path = r'C:\Users\RogerWu\Downloads\Temp'
 print("Today's Date: ", datetime.now())
 
-
-number_of_days_old = 81
+number_of_days_old = 45
 cutoff_date = datetime.now() - timedelta(number_of_days_old)
 
 print("Cutoff_date: ", cutoff_date)
@@ -15,14 +18,15 @@ time_in_secs = time.time() - (number_of_days_old * 24 * 60 * 60)
 
 os.chdir(dest_path)
 print("Current working directory : ", os.getcwd())
+print("")
 
 for item in os.listdir():
     if os.path.isfile(item):
         print("File name : ", item)
-        print("os.stat : ", os.stat(item))
-        print("os.stat.st_ctime : ", os.stat(item).st_ctime)
-        print("st_ctime datetime : ", datetime.fromtimestamp(os.stat(item).st_ctime))
+        print("os.stat : ", os.stat(item))                                                  # stats information about the file
+        print("os.stat.st_ctime : ", os.stat(item).st_ctime)                                # create date of the file (in seconds)
+        print("st_ctime datetime : ", datetime.fromtimestamp(os.stat(item).st_ctime))       # create date of the file (in datetime format)
         print("")
         
- #      if datetime.fromtimestamp(os.stat(item).st_ctime) > time_in_secs:
-#            os.remove(item)
+        if datetime.fromtimestamp(os.stat(item).st_ctime) > cutoff_date:
+            os.remove(item)
