7abcb18d64562ad69d5ad5f1f00f0f1f5fcecdca
Roger Wu <roger_wu@hotmail.com>
2020-05-21 22:31:43 -0600

    init

---
+++ file_size_search.py
@@ -0,0 +1,14 @@
+# this program will search and list files smaller than a specified byte size
+
+import os
+from datetime import datetime, timedelta
+import time
+
+dest_path = r'F:\temp'
+
+file_size_limit = 1000
+
+os.chdir(dest_path)
+
+for item in os.listdir():
+    if os.path.isfile(item):
